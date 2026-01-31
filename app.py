22a61cea0be24898e05c0cbfd9f5a8114cfd68f8
Jannik Hinkelmann <jannik.hinkelmann@gmx.de>
2026-01-31 12:17:07 +0100

    Add ProxyFix to WSGI application

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ logger.info("Starting application...")
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('secretkey')
 app.config.from_object(Config)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 
 db.init_app(app)
 
